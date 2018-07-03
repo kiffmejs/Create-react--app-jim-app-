ea701d5b21ecd7dd4611f07454623c6199411160
Jim Kiffmeyer <kiffmejs@mail.uc.edu>
2018-07-03 09:52:00 -0400

    Add Sidebar and Chat Components

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
+import Sidebar from './Sidebar'
+import Chat from './Chat'
+import { SIGUNUSED } from 'constants';
+
 class Main extends Component {
     render () {
         return (
         <div className="Main">
-        Main
+        <Sidebar />
+        <Chat />
         </div>
         )
     }
